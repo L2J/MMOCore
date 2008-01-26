e51d1444681c24e92a55c7cbb60683582babc13a
KenM <KenM@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-01-26 17:56:30 +0000

    Same as [832]. Fix was lost on merge.

--- MMOCore/src/org/mmocore/network/SelectorThread.java
+++ MMOCore/src/org/mmocore/network/SelectorThread.java
@@ -322,7 +322,6 @@ public class SelectorThread<T extends MMOClient> extends Thread
                 }
                 else
                 {
-                    key.cancel();
                     sc.socket().close();
                 }
             }
